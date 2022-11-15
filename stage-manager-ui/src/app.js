6dca55eba4933b9db93b5d8bc6d0b94d2f3771b7
Onza22 <christian.s@hotmail.co.uk>
2022-11-15 00:22:26 +0000

    Routing and some pages

--- stage-manager-ui/src/app.js
+++ stage-manager-ui/src/app.js
@@ -1,6 +1,19 @@
 require('bootstrap/dist/css/bootstrap.min.css')
 require('bootstrap')
 
+import { PLATFORM } from "aurelia-framework";
+
 export class App {
   title = 'Stage Runner';
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'],       name: 'home',       moduleId: PLATFORM.moduleName('home/index'), nav: true, title: 'Home' },
+      { route: ['register'],       name: 'register',       moduleId: PLATFORM.moduleName('auth/register'),nav: true, title: 'Register' },
+      { route: ['login'],       name: 'login',       moduleId: PLATFORM.moduleName('auth/login'), nav: true, title: 'Login' },      
+      { route: ['running-order'],       name: 'running-order',       moduleId: PLATFORM.moduleName('running-order/index'), nav: true, title: 'Running Order' },            
+    ]);
+    console.log(router);
+  }
 }
--- stage-manager-ui/src/navigation.js
+++ stage-manager-ui/src/navigation.js
@@ -1,7 +1,6 @@
 import{bindable}from "aurelia-framework"
 export class Navigation {
   @bindable title;
-  constructor(title){
-    this.title = title;
-  }
+  @bindable router;
+  
 }
