4432d943263c630f76f8f1afe12964eaaf103b6c
Onza22 <christian.s@hotmail.co.uk>
2022-11-21 23:57:35 +0000

    menu twerks

--- stage-manager-ui/src/navigation.js
+++ stage-manager-ui/src/navigation.js
@@ -1,5 +1,6 @@
-import { bindable, inject } from "aurelia-framework"
+import { bindable, inject } from "aurelia-framework";
 import { AppState } from "./AppState";
+import { Collapse } from "bootstrap";
 
 @inject(AppState)
 export class Navigation {
@@ -12,15 +13,25 @@ export class Navigation {
 
   navLinks() {
     const isAuth = !!this.appState.user;
-    const links = this.router.navigation.filter(link => {
-      if (link.settings.show === 'auth') {
+    const links = this.router.navigation.filter((link) => {
+      if (link.settings.show === "auth") {
         return isAuth;
       }
-      if (link.settings.show === 'noauth') {
+      if (link.settings.show === "noauth") {
         return !isAuth;
       }
       return true;
     });
     return links;
+  }
+
+  closeNavBar() {
+    console.log("sdasadadsd");
+    const menuToggle = document.getElementById("navbarsExample01");
+    const bsCollapse = new Collapse(menuToggle, {toggle: false}); 
+    if (menuToggle.classList.contains("show")) {
+      bsCollapse.toggle();
+    }
+    return true;
   }
 }
